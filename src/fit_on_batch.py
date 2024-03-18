Fix half precision training using bf16

src/fit_on_batch.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 import torch
 from tqdm import trange, tqdm
+from cprint import c_print
 
 from trainer import Trainer, get_data_loader
 from utils import set_seed, load_params_from_file, get_available_device
@@ -33,7 +34,7 @@ def test_generate(model: MultivariateTimeLLM, cfg, batch):
     model.generate(states, diffs, bc_mask, position_ids, N_patch, show_num=1)
 
 
-def run_train_epoch(dataloader, trainer: Trainer, optimizer, batch):
+def run_train_epoch(trainer: Trainer, optimizer, batch):
     trainer.model.train()
 
     states, diffs, bc_mask, position_ids = batch
@@ -63,14 +64,15 @@ if __name__ == '__main__':
     logging.info(f"Parameters for training: {training_params}")
 
     # Get the model
-    precision = torch.float16 if training_params['half_precision'] else torch.float32
-    model = MultivariateTimeLLM(training_params, device_map=get_available_device()).to(precision)
+    precision = torch.bfloat16 if training_params['half_precision'] else torch.float32
+    model = MultivariateTimeLLM(training_params, device_map=get_available_device(), precision=precision)
 
     # Get the train data loader
     train_dataloader = get_data_loader(training_params)
 
     trainer = Trainer(params=training_params,
                       model=model,
+                      precision=precision,
                       device=get_available_device())
 
     optimizer = trainer.prepare_optimizers()
@@ -78,11 +80,11 @@ if __name__ == '__main__':
     batch = next(iter(train_dataloader))
 
     for epoch in trange(1000, desc="Training"):
-        train_log_metrics = run_train_epoch(dataloader=train_dataloader,
-                                            trainer=trainer,
+        train_log_metrics = run_train_epoch(trainer=trainer,
                                             optimizer=optimizer,
                                             batch=batch)
 
-        logging.info(f'[TRAIN]: Epoch [{epoch + 1}] Metrics: {train_log_metrics}')
+        if epoch % 5 == 0:
+            logging.info(f'[TRAIN]: Epoch [{epoch + 1}] Metrics: {train_log_metrics}')
 
     test_generate(model, training_params, batch)

src/main.py
@@ -29,7 +29,7 @@ def test_generate(model: MultivariateTimeLLM, cfg):
     N_patch = ds.N_patch
 
     if cfg['multiprocess']:
-        dl = ParallelDataGenerator(ds, bs=1)
+        dl = ParallelDataGenerator(ds, bs=1, num_procs=1)
         dl.run()
     else:
         dl = SingleDataloader(ds, bs=1)
@@ -85,14 +85,15 @@ if __name__ == '__main__':
     logging.info(f"Parameters for training: {training_params}")
 
     # Get the model
-    precision = torch.float16 if training_params['half_precision'] else torch.float32
-    model = MultivariateTimeLLM(training_params, device_map=get_available_device()).to(precision)
+    precision = torch.bfloat16 if training_params['half_precision'] else torch.float32
+    model = MultivariateTimeLLM(training_params, device_map=get_available_device(), precision=precision)
 
     # Get the train data loader
     train_dataloader = get_data_loader(training_params)
 
     trainer = Trainer(params=training_params,
                       model=model,
+                      precision=precision,
                       device=get_available_device())
 
     optimizer = trainer.prepare_optimizers()

src/models/model.py
@@ -17,7 +17,7 @@ transformers.logging.set_verbosity_error()
 
 
 class MultivariateTimeLLM(nn.Module):
-    def __init__(self, config, device_map='cpu'):
+    def __init__(self, config, device_map='cpu', precision=torch.float32):
         super().__init__()
 
         self.config = config
@@ -39,7 +39,7 @@ class MultivariateTimeLLM(nn.Module):
             trust_remote_code=True,
             local_files_only=False,
             config=self.llm_config,
-            torch_dtype=torch.float16 if config['half_precision'] else torch.float32,
+            torch_dtype=precision,
             load_in_4bit=config['llm_4bit_loading'],
             device_map=device_map
         )
@@ -64,26 +64,22 @@ class MultivariateTimeLLM(nn.Module):
         self.N, self.M = config["patch_size"]
         self.patch_in_dim = self.N * self.M * 3
 
-        # Adjust the backbone for time series task
         self.input_embeddings = InputEmbeddings(self.patch_in_dim,
                                                 self.llm_in_dim,
                                                 self.llm_config.embd_pdrop,
                                                 self.llm_config.layer_norm_epsilon,
                                                 self.llm_config.max_position_embeddings)
-
-        if config['half_precision']:
-            self.input_embeddings.to(torch.float16)
+        self.input_embeddings.to(precision)
 
         self.output_layer = PatchDecoder(self.llm_in_dim, self.patch_in_dim)
+        self.output_layer.to(precision)
 
-        if config['half_precision']:
-            self.output_layer.to(torch.float16)
-
+        # Adjust the backbone for time series task
         self._adjust_backbone()
         self.to(device_map)
 
         self.device_map = device_map
-        self.precision = torch.float16 if config['half_precision'] else torch.float32
+        self.precision = precision
 
     def _adjust_backbone(self):
         # Nullify undesired layers

src/trainer.py
@@ -8,6 +8,7 @@ from dataloader.MGN_dataloader import MGNSeqDataloader
 from dataloader.parallel_dataloader import ParallelDataGenerator, SingleDataloader
 from utils import get_available_device, get_trainable_parameters
 from losses import MSELoss, RMSELoss, MAELoss, MAPELoss, SMAPELoss
+from models.model import MultivariateTimeLLM
 
 
 def get_data_loader(config):
@@ -28,7 +29,7 @@ def get_data_loader(config):
 
 
 class Trainer:
-    def __init__(self, params, model, device=get_available_device()):
+    def __init__(self, params, model: MultivariateTimeLLM, precision, device=get_available_device()):
         """
         params (dict): A dict with the configuration parameters (e.g., learning rate, optimizer, etc.)
         """
@@ -38,7 +39,7 @@ class Trainer:
         self.model = model
         self.loss_fn = self._get_loss_fn(params['loss_function'])
 
-        self.precision = torch.float16 if params['half_precision'] else torch.float32
+        self.precision = precision
         self.device = device
 
     def calculate_loss(self, preds: torch.Tensor, diffs: torch.Tensor, bc_mask: torch.Tensor):
@@ -71,7 +72,7 @@ class Trainer:
         elif optimizer_type == "adam":
             optimizer = torch.optim.Adam(list(params),
                                          lr=self.params['learning_rate'],
-                                         weight_decay=self.params['weight_decay'])
+                                         weight_decay=self.params['weight_decay'], eps=1e-7)
         elif optimizer_type == "sgd":
             optimizer = torch.optim.SGD(list(params),
                                         lr=self.params['learning_rate'],