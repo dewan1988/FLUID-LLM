44fffce4c10cb94bf782f8d0a97bc7d2a9daeae4
bubbles <mz406@cam.ac.uk>
2024-03-20 20:28:32 +0000

    Load tfrecrod in pytorch

---
+++ ds_download/torch_MGN.py
@@ -0,0 +1,80 @@
+""" Load MGN tfrecord dataset in pytorch """
+
+import json
+import os
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchdata.datapipes.iter import FileLister, FileOpener
+
+def check_repeat(arr):
+    first_slice = arr[0]
+    for i in range(1, len(arr)):
+        if not np.array_equal(first_slice, arr[i]):
+            return False
+    return True
+
+def parse_numerical_data(proto, meta):
+    """Parses numerical data from a format equivalent to tf.Example, based on 'meta.json'."""
+    out = {}
+    for key, field in meta['features'].items():
+        if field['dtype'] == 'float32':
+            dtype = np.float32
+        elif field['dtype'] == 'int64':
+            dtype = np.int64
+        elif field['dtype'] == 'int32':
+            dtype = np.int32
+
+        else:
+            raise ValueError(f"Unsupported dtype: {field['dtype']}")
+        data = np.frombuffer(proto[key][0], dtype=dtype).reshape(field['shape'])
+        if field['type'] == 'static':
+            data = np.tile(data, (meta['trajectory_length'], 1, 1))
+        elif field['type'] == 'dynamic_varlen':
+            # Example handling for dynamic variable length data
+            length = np.frombuffer(proto['length_' + key], dtype=np.int32)
+            data = [data[i:i + l] for i, l in enumerate(length)]  # This will create a list of arrays
+        elif field['type'] != 'dynamic':
+            raise ValueError('Invalid data format')
+
+        out[key] = torch.tensor(data, dtype=torch.__dict__[field['dtype']])
+    return out
+
+
+def main():
+    dataset_path = '/mnt/hdd1/fluid_ds/meshgraphnets/cylinder'
+    split_name = 'train'
+    with open(os.path.join(dataset_path, 'meta.json'), 'r') as fp:
+        meta = json.load(fp)
+
+    datapipe1 = FileLister(dataset_path, f'{split_name}.tfrecord')
+    datapipe2 = FileOpener(datapipe1, mode="b")
+    tfrecord_loader_dp = datapipe2.load_from_tfrecord()
+
+    for i, example in enumerate(tfrecord_loader_dp):
+        # Process each example here
+        # The structure of 'example' depends on the content of your TFRecord files
+        sample = parse_numerical_data(example, meta)
+        print(sample.keys())
+
+        # Unmodified save data
+        save_stats = {"velocity": sample['velocity'], "pressure": sample['pressure']}
+
+        # Remove duplicate for static entries
+        static_vars = ['cells', 'mesh_pos', 'node_type']
+        for var in static_vars:
+            if not check_repeat(sample[var]):
+                print("Warning: Not repeated")
+                print(f'{var}: {check_repeat(sample[var])}')
+
+            save_stats[var] = sample[var][0]
+
+        # Convert cells to int16
+        if torch.all(save_stats['cells'] >= np.iinfo(np.int16).min) and torch.all(save_stats['cells'] <= np.iinfo(np.int16).max):
+            save_stats['cells'] = save_stats['cells'].to(torch.int16)
+
+
+
+
+if __name__ == "__main__":
+    main()
--- src/models/layers/patch_decoder.py
+++ src/models/layers/patch_decoder.py
@@ -7,10 +7,10 @@ class PatchDecoder(nn.Module):
     def __init__(self, llm_dim, out_dim, hid_dim=256):
         super().__init__()
 
-        self.dec_in = nn.Linear(llm_dim, hid_dim)
-        self.dec_out = nn.Linear(hid_dim, out_dim)
+        # self.dec_in = nn.Linear(llm_dim, hid_dim)
+        self.dec_out = nn.Linear(llm_dim, out_dim)
 
     def forward(self, x):
-        x = self.dec_in(x)
+        # x = self.dec_in(x)
         z = self.dec_out(x)
         return z
