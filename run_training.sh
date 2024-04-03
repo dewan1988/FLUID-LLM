645af6e3be19bf45e115b4439a35ebb983763458
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2024-04-03 18:32:10 +0100

    Up

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,5 @@
 CONFIG_PATH="configs/training1.yaml"
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
-# Down here, mixed_precision can be [bf16, fp16, fp32, no]
+# Down here, mixed_precision can be [bf16, fp16, fp8, no]
 # --use_deepspeed
-accelerate launch --mixed_precision=bf16 --gradient_accumulation_steps=4 src/main.py --config_path=$CONFIG_PATH
+accelerate launch --mixed_precision=no src/main.py --config_path=$CONFIG_PATH
