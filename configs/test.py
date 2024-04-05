c27140e8cc07f9326f394ed42f73d9d0f592d7cc
bubbles <mz406@cam.ac.uk>
2024-04-05 03:27:15 +0100

    Move around unneeded files

--- configs/test.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-cfg_path = 'training1.yaml'
-save_path = '/dev/null/'  # Corrected path without the trailing slash
-
-# Correct usage to copy and discard the file content
-shutil.copyfile(cfg_path, save_path)
-
