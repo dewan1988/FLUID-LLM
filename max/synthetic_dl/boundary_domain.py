076c65b5be11a2e49b63ae7b4dcf38cc22bbb929
Max Zhu <mz406@cam.ac.uk>
2024-04-09 05:05:21 +0100

    Synth dl2

--- max/synthetic_dl/boundary_domain.py
+++ max/synthetic_dl/boundary_domain.py
@@ -37,50 +37,72 @@ class BoundaryConditionGenerator:
         Inside the ellipse is 0, outside is 1.
         """
         # Create a meshgrid of coordinates x, y
-        y, x = np.ogrid[:self.ny, :self.nx]
+        # x, y = np.ogrid[:self.nx, :self.ny]
+        x = np.linspace(0, self.nx, self.nx)
+        y = np.linspace(0, self.ny, self.ny)
+        X, Y = np.meshgrid(x, y, indexing='ij')
 
-        margin = max(self.nx, self.ny) / 2
+        if self.nx > self.ny:
+            max_ax = self.nx / 2
+            min_ax = self.ny / 2
+        else:
+            max_ax = self.ny / 2
+            min_ax = self.nx / 2
+
         # Center of the ellipse
-        x0 = np.random.uniform(margin * 0.9, (self.nx - margin) * 1.1)
-        y0 = np.random.uniform(margin * 0.9, (self.ny - margin) * 1.1)
+        maj_cent = np.random.uniform(max_ax * 0.9, (self.nx - max_ax) * 1.1)
+        min_cent = np.random.uniform(min_ax * 0.9, (self.ny - min_ax) * 1.1)
 
         # Semi-major and semi-minor axes (ensure a > b for convention)
-        a = np.random.uniform(margin / 1.5, margin)
-        b = np.random.uniform(margin / 2, a)
+        a = np.random.uniform(max_ax / 1.33, max_ax*1.1)
+        b = np.random.uniform(min_ax / 1.33, min_ax*1.1)
 
         # Orientation angle in degrees
-        theta = np.random.uniform(0, 360)
+        theta = np.random.uniform(-30, 30)
 
         # Calculate the ellipse equation
         theta_rad = np.deg2rad(theta)  # Convert theta to radians for np.cos and np.sin
         cos_theta = np.cos(theta_rad)
         sin_theta = np.sin(theta_rad)
 
-        term1 = ((x - x0) * cos_theta + (y - y0) * sin_theta) ** 2 / a ** 2
-        term2 = ((x - x0) * sin_theta - (y - y0) * cos_theta) ** 2 / b ** 2
+        if self.nx > self.ny:
+            term1 = ((X - maj_cent) * cos_theta + (Y - min_cent) * sin_theta) ** 2 / a ** 2
+            term2 = ((X - maj_cent) * sin_theta - (Y - min_cent) * cos_theta) ** 2 / b ** 2
+        else:
+            term1 = ((X - maj_cent) * cos_theta - (Y - min_cent) * sin_theta) ** 2 / b ** 2
+            term2 = ((X - maj_cent) * sin_theta + (Y - min_cent) * cos_theta) ** 2 / a ** 2
 
         # Inside the ellipse <= 1, so we invert the condition to set inside to 0, outside to 1
-        ellipse = 1 - (term1 + term2 <= 1).astype(int)
-
+        ellipse = 1 - (term1 + term2 <= 1).astype(int)  # .T
+        # Set boundary to 1 always
+        ellipse[0, :] = 1
+        ellipse[-1, :] = 1
+        ellipse[:, 0] = 1
+        ellipse[:, -1] = 1
         return ellipse.astype(bool)
 
-    def random_polygon_boundary(self, n_points=15):
+    def random_polygon_boundary(self, n_points=20):
         """
         Generates a mask for a random polygon boundary defined by a convex hull of 'n_points' points.
         """
-        points = np.random.rand(n_points, 2) * [self.nx, self.ny]
-        hull = ConvexHull(points)
+        rand_points = np.random.rand(n_points, 2) * [self.nx, self.ny]
+        hull = ConvexHull(rand_points)
 
         # Create a path from the convex hull
-        path = mpath.Path(points[hull.vertices])
+        path = mpath.Path(rand_points[hull.vertices])
 
         # Rasterize the path to a grid
-        X, Y = np.meshgrid(np.arange(self.nx), np.arange(self.ny))
+        X, Y = np.meshgrid(np.arange(self.nx), np.arange(self.ny), indexing='ij')
         points = np.vstack((X.ravel(), Y.ravel())).T
         grid = path.contains_points(points)
         mask = grid.reshape((self.nx, self.ny)).astype(int)
 
-        # Invert mask if necessary so the inside of the polygon is 1, outside is 0
+        # print(mask.shape)
+        # plt.imshow(mask.T, origin='lower')
+        # plt.scatter(rand_points[:, 0], rand_points[:, 1], c='r')
+        # plt.show()
+        # exit(5)
+        # Invert mask if necessary so the outside of the polygon is 1, inside is 0
         return (1 - mask).astype(bool)
 
 
