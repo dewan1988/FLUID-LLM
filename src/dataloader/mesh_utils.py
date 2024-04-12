37c0389d52a5ef7c8d1295c0917dac9c9cead1d4
Your Name <you@example.com>
2024-04-12 02:38:40 +0100

    Update plot_patches

--- src/dataloader/mesh_utils.py
+++ src/dataloader/mesh_utils.py
@@ -9,58 +9,13 @@ import matplotlib
 # Monkey patch plt grid interpolator.
 from _triinterpolate import TriInterpolator as CustomTriInterpolator
 from _triinterpolate import LinearTriInterpolator as CustomLinearTriInterpolator
+
 # Replace the default interpolator with the custom one
 matplotlib.tri.TriInterpolator = CustomTriInterpolator
 matplotlib.tri.LinearTriInterpolator = CustomLinearTriInterpolator
 
 from matplotlib import tri as mtri
 import matplotlib.pyplot as plt
-
-
-# def plot_mesh(pos, faces, val):
-#     """Plots triangular mesh from positions of nodes and triangles defined using faces."""
-#     triang = mtri.Triangulation(pos[:, 0], pos[:, 1], faces)
-#
-#     plt.figure(figsize=(10, 4))
-#     plt.tripcolor(triang, val)  # or contourf
-#     plt.ylim([0., 0.5])
-#     plt.axis("equal")
-#
-#     # plt.axis("off")
-#     plt.tight_layout()
-#
-#     plt.show()
-#
-#
-# def plot_mesh_smooth(pos, val, faces, grid_res):
-#     """Plots a 2D image from positions of nodes and values of the nodes."""
-#     grid_z, _ = to_grid(pos, val, faces, grid_res)
-#     plt.figure(figsize=(12, 4))
-#     plt.imshow(grid_z.T, extent=(np.min(pos[:, 0]), np.max(pos[:, 0]), np.min(pos[:, 1]), np.max(pos[:, 1])),
-#                origin='lower', cmap='viridis')
-#
-#     plt.tight_layout()
-#     plt.show()
-#
-#
-# def inpaint(img, mask, method='telea'):
-#     """Inpaints mask to avoid rough edges. """
-#
-#     min_val = np.min(img)
-#     max_val = np.max(img)
-#
-#     img = (img - min_val) / (max_val - min_val)
-#     img = (img * 255).astype(np.uint8)
-#     mask = (mask * 255).astype(np.uint8)
-#     if method == 'telea':
-#         img = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
-#     elif method == 'ns':
-#         img = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
-#     else:
-#         raise ValueError(f"Unknown inpainting method: {method}")
-#
-#     img = (img / 255) * (max_val - min_val) + min_val
-#     return img.astype(np.float32)
 
 
 def plot_patches(state: torch.Tensor, N_patch: tuple):
@@ -71,18 +26,20 @@ def plot_patches(state: torch.Tensor, N_patch: tuple):
     # state.clamp_(0, 1)
     state = state.detach().float().cpu().numpy()
     v_min, v_max = state.min(), state.max()
+    print(f'min: {v_min:.2g}, max: {v_max:.2g}, mean: {state.mean():.2g}, std: {state.std():.2g}')
+
     # Normalize state to [0, 1]
     state = (state - v_min) / (v_max - v_min)
-    print(f'min: {v_min:.2g}, max: {v_max:.2g}, mean: {state.mean():.2g}, std: {state.std():.2g}')
 
     fig, axes = plt.subplots(y_count, x_count, figsize=(16, 4))
     for i in range(y_count):
         for j in range(x_count):
             patch = state[i + j * y_count].T
-            axes[i, j].imshow(patch, vmin=v_min, vmax=v_max)
+            axes[i, j].imshow(patch, vmin=0, vmax=1)
             axes[i, j].axis('off')
     plt.tight_layout()
     plt.show()
+
 
 def plot_full_patches(state, N_patch: tuple, ax):
     """ Plot patches as a single image"""
@@ -170,6 +127,51 @@ def test_time(save_no=1):
 
     return Vx_interp
 
+
+# def plot_mesh(pos, faces, val):
+#     """Plots triangular mesh from positions of nodes and triangles defined using faces."""
+#     triang = mtri.Triangulation(pos[:, 0], pos[:, 1], faces)
+#
+#     plt.figure(figsize=(10, 4))
+#     plt.tripcolor(triang, val)  # or contourf
+#     plt.ylim([0., 0.5])
+#     plt.axis("equal")
+#
+#     # plt.axis("off")
+#     plt.tight_layout()
+#
+#     plt.show()
+#
+#
+# def plot_mesh_smooth(pos, val, faces, grid_res):
+#     """Plots a 2D image from positions of nodes and values of the nodes."""
+#     grid_z, _ = to_grid(pos, val, faces, grid_res)
+#     plt.figure(figsize=(12, 4))
+#     plt.imshow(grid_z.T, extent=(np.min(pos[:, 0]), np.max(pos[:, 0]), np.min(pos[:, 1]), np.max(pos[:, 1])),
+#                origin='lower', cmap='viridis')
+#
+#     plt.tight_layout()
+#     plt.show()
+#
+#
+# def inpaint(img, mask, method='telea'):
+#     """Inpaints mask to avoid rough edges. """
+#
+#     min_val = np.min(img)
+#     max_val = np.max(img)
+#
+#     img = (img - min_val) / (max_val - min_val)
+#     img = (img * 255).astype(np.uint8)
+#     mask = (mask * 255).astype(np.uint8)
+#     if method == 'telea':
+#         img = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+#     elif method == 'ns':
+#         img = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+#     else:
+#         raise ValueError(f"Unknown inpainting method: {method}")
+#
+#     img = (img / 255) * (max_val - min_val) + min_val
+#     return img.astype(np.float32)
 
 if __name__ == "__main__":
     import pickle
