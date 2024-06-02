361a8ef23c85255e6d1dbb0724c037fe65c021f6
Max Zhu <mz406@cam.ac.uk>
2024-06-03 00:58:01 +0100

    Add cylinder MGN

--- eagle/eval_mgn.py
+++ eagle/eval_mgn.py
@@ -14,7 +14,7 @@ torch.set_float32_matmul_precision('medium')
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_path', default='/home/bubbles/Documents/LLM_Fluid/ds/MGN/cylinder_dataset/', type=str, help="Dataset location")
 parser.add_argument('--n_processor', default=15, type=int, help="Number of chained GNN layers")
-parser.add_argument('--name', default='mgn_test', type=str, help="Name for saving/loading weights")
+parser.add_argument('--name', default='cylinder', type=str, help="Name for saving/loading weights")
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,7 +25,7 @@ BATCHSIZE = 1
 @torch.inference_mode()
 def evaluate():
     print(args)
-    length = 101
+    length = 251
     dataset = EagleMGNDataset(args.dataset_path, mode="test", window_length=length, with_cluster=False, normalize=False, with_cells=True)
 
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
@@ -52,10 +52,10 @@ def evaluate():
         rmse = get_nrmse(state, state_hat, mesh_pos, x['cells'])
         rmses.append(rmse.numpy())
 
-        print(f'{mesh_pos.shape = }, {faces.shape = }, {state_hat.shape = }, {state.shape = }')
-        plot_final(mesh_pos[0, 0], faces[0, 0], state_hat[0], state[0])
-
-        exit(7)
+        # print(f'{mesh_pos.shape = }, {faces.shape = }, {state_hat.shape = }, {state.shape = }')
+        # plot_final(mesh_pos[0, 0], faces[0, 0], state_hat[0], state[0])
+        #
+        # exit(7)
 
     rmses = np.concatenate(rmses)
     print(f'{rmses.mean(axis=0).tolist()}')
