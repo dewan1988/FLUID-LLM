be45a335f8d2787c14d9e74f4f8b9748baabdb47
dewanjafor <mdewan@ncsu.edu>
2025-03-31 12:30:31 -0400

    Type your commit message here

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-conda env create -n llm4multivariatets -f environemnt.yml python=3.10
-conda activate llm4multivariatets
+conda env create -n llm -f environemnt.yml python=3.10
+conda activate llm
